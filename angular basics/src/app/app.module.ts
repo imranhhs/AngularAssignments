903cde1bc919ba8a6e4034db4d7942c6a71931cc
Imran Shaikh <imran.shaikh@indegene.com>
2020-04-24 03:12:31 +0530

    # Basic File Structure Setup

--- angular basics/src/app/app.module.ts
+++ angular basics/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { ProductComponent } from "./component";
+import { TrainBookingComponent } from './train-booking/train-booking.component';
+import { AppComponent } from "./old/app.component";
 
 @NgModule({
     imports: [BrowserModule],
-    declarations: [ProductComponent],
-    bootstrap: [ProductComponent]
+    declarations: [AppComponent, ProductComponent, TrainBookingComponent],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ angular basics/src/app/train-booking/train-booking.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-train-booking',
+  templateUrl: './train-booking.component.html',
+  styleUrls: ['./train-booking.component.css']
+})
+export class TrainBookingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
